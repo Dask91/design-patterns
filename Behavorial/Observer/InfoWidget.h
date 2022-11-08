5e998d40b13e862f1c8cdb664a78fd32de212037
Simon Grenier-Corriveau <sgreniercorriveau@gmail.com>
2022-11-07 19:20:48 -0500

    Removed comment lines in InfoWidget.

--- Behavorial/Observer/InfoWidget.h
+++ Behavorial/Observer/InfoWidget.h
@@ -1,9 +1,8 @@
 #pragma once
 
 
-// A simple interface for an "information" widget that can be displayed on screen.
-// This interface is superficial in this example, as it would be more direct to handle a Fl_Widget with draw().
-// The InfoWidget interface shows what a typical base class for an observer can look like.
+// A simple interface for a widget that can be displayed on screen.
+// The interface is superfluous in this example, as it would be more direct to handle a Fl_Widget with draw().
 class InfoWidget
 {
 public:
